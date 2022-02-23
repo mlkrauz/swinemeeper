c2ec5c73a8b7ab4563403cf0f33ffcec072293b2
mlkrauz <AnElusiveEntity@gmail.com>
2022-02-23 12:55:26 -0500

    Updated pre-save validation hooks.

--- backend/src/models/Game/Row.ts
+++ backend/src/models/Game/Row.ts
@@ -1,5 +1,6 @@
 import { Schema, Model, Types } from 'mongoose'
 import { Tile, tileSchema } from './Tile'
+import { BoardSize, BoardStats } from '../../../../shared/defs'
 
 /**
  * A row of minesweeper Tiles.
@@ -21,3 +22,17 @@ export const rowSchema = new Schema<Row, Model<Row>>(
   }, 
   { _id: false }
 )
+
+/**
+ * The quanitity of tiles should be static after creation, as board size cannot change.
+ * Here we check that 
+ */
+rowSchema.pre('save', function(): void {
+  if (this.isModified('tiles') | this.isNew()) {
+    
+    // Get the parent game's board size.
+    const size: BoardSize = this.parent().get('boardSize')
+
+    // Use this to
+  }
+})
--- backend/src/schemas/typeDefs.ts
+++ backend/src/schemas/typeDefs.ts
@@ -1,6 +1,44 @@
 import { gql } from 'apollo-server-express'
 
 export const typeDefs = gql`
+
+  # # # # # # # # # # # # # # #
+  # ENUMS
+  # # # # # # # # # # # # # # #
+  enum TileType {
+    UNCOVERED_0
+    UNCOVERED_1
+    UNCOVERED_2
+    UNCOVERED_3
+    UNCOVERED_4
+    UNCOVERED_5
+    UNCOVERED_6
+    UNCOVERED_7
+    UNCOVERED_8
+    COVERED
+    FLAGGED
+    FLAGGED_WRONG
+    MINE_REVEALED
+    MINE_CLICKED
+  }
+
+  enum GameState {
+    INPROGRESS
+    PAUSED
+    WIN
+    LOSS
+    ABANDONED
+  }
+
+  enum BoardSize {
+    BEGINNER
+    INTERMEDIATE
+    EXPERT
+  }
+
+  # # # # # # # # # # # # # # #
+  # TYPES
+  # # # # # # # # # # # # # # #
   type User {
     _id: ID!
     username: String!
