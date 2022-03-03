02fce1fc1793ca18045ee371527df98135f434ce
mlkrauz <AnElusiveEntity@gmail.com>
2022-03-03 18:04:09 -0500

    Began implementation of the game.

---
+++ frontend/src/components/Game/Tile/Tile.tsx
@@ -0,0 +1,7 @@
+export const Tile = () => {
+  
+  return (
+    <button style={{ width: '56px', height: '56px' }}>
+    </button>
+  )
+}
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Container } from '../components/Container/Container'
+import { Swinemeeper } from '../components/Game/Swinemeeper'
 
 export const Home: React.FC = () => {
   return (
@@ -14,7 +15,9 @@ export const Home: React.FC = () => {
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et tortor consequat id porta nibh venenatis cras. Porta nibh venenatis cras sed felis. Ac felis donec et odio pellentesque diam volutpat. Duis at tellus at urna condimentum mattis pellentesque id. Lacinia quis vel eros donec ac odio. Congue nisi vitae suscipit tellus. Velit euismod in pellentesque massa placerat duis ultricies lacus. Odio facilisis mauris sit amet. Nibh sed pulvinar proin gravida hendrerit lectus. Viverra orci sagittis eu volutpat.
         </p>
       </Container>
-      <Container></Container>
+      <Container>
+        <Swinemeeper />
+      </Container>
     </>
   )
 }
